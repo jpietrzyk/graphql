c0d910349e4d0bfd4cb07608457088182e4d65a8
Jacek Pietrzyk <jacpie3k@gmail.com>
2019-08-20 08:42:22 +0200

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+5.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+
+  5.times do
+    user.books.create(title: Faker::Book.title)
+  end
+end
